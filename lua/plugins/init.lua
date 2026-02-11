74f265ea926604d4e62ef3d2fb747083e7c63749
Peter Thor <83021617+peterthor1@users.noreply.github.com>
2026-02-11 17:55:25 -0500

    feat: implement automatic installation of mason packages

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    opts = require "configs/conform",
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -13,17 +13,36 @@ return {
     end,
   },
   {
-  'mrcjkb/rustaceanvim',
-  version = '^7',
-  lazy = false,
-},
-{
-  'rust-lang/rust.vim',
-  ft = "rust",
-  init = function ()
-    vim.g.rustfmt_autosave = 1
-  end
-},
+    "mrcjkb/rustaceanvim",
+    version = "^7",
+    lazy = false,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+        "html-lsp",
+        "lua-language-server",
+        "prettierd",
+        "rust-analyzer",
+        "sqls",
+        "stylua",
+        "vtsls",
+        "vue-language-server",
+      },
+      run_on_start = true,
+    },
+    lazy = false,
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
